37b62fadcabfd35cd0dc868164935d8238b467e4
Philip Rhinow <philipprhinowgmail.com>
2025-05-14 18:07:53 +0200

    fertig

--- Uni/Aufgabe15.c
+++ Uni/Aufgabe15.c
@@ -8,34 +8,35 @@ int main(void)
     int i = 0;
     int eingabe = 0;
     int primfak = 0;
-    int faktoren[10];
+    int faktoren[10] = {0,0,0,0,0,0,0,0,0,0};
     int primzahl[30] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47};
 
     printf("Bitte geben sie eine Zahl zwischen 2 und 100 ein: ");
     scanf("%d", &eingabe);
 
     int zwischen = eingabe;
+    int index = 0;
 
 
     // bespiel 27
 
-    for (int i = 1; i < 20; i++)
+    for (int i = 0; i < 20; i++)
     {
-        i--;
+        
         if (zwischen % primzahl[i] == 0)
         {
-            faktoren[i] = primzahl[i]; //schreibt die Primzahl in das array
+            
+            faktoren[index++] = primzahl[i]; 
             zwischen = zwischen / primzahl[i];
-            i = 0;
-            if (zwischen == 0)
-            {
-                break;
-            }
-
+            i = -1;
         }
+        if (zwischen == 1)
+        {
+            break;
+        }       
     }
 
-    for (int y = 0; y < 10; y++)
+    for (int y = 0; y < index; y++)
     {
         printf("%d\n", faktoren[y]);
     }
