ab2d65024e8d772888f9b61088ba7e06443d40e6
Philip Rhinow <philipprhinow@gmail.com>
2025-05-22 09:05:36 +0200

    zwischen

---
+++ Uni/Aufgabe17.c
@@ -0,0 +1,33 @@
+/*
+Aufgabe 17: Benzinverbrauch
+Im Folgenden sollen zwei Funktionen zum Thema Benzinverbrauch bei Automobilen entworfen
+werden.
+• Die Funktion „Spritverbrauch“ bekommt die gefahrenen Kilometer und den Normverbrauch
+des Fahrzeugs pro 100 km übergeben. Rückgabewert ist die Menge an Benzin in Litern, die
+verbraucht worden ist.
+• In der zweiten Funktion „Tankinhalt“ soll die Restmenge von Benzin im Tank berechnet
+werden. Übergabeparameter sind wieder die Anzahl der gefahrenen Kilometer und der
+Normverbrauch des Fahrzeugs pro 100 km und zusätzlich der maximale Tankinhalt.
+Vervollständigen Sie die beiden Funktionen und schreiben Sie ein Hauptprogramm, das die beiden
+Funktionen aufruft. Verwenden Sie für alle Variablen den Datentyp double.
+*/
+#include <stdio.h>
+
+double Spritverbrauch(double, double);
+double Tankinhalt(double, double, double);
+
+int main(void)
+{
+    printf("Bitte geben sie");
+
+}
+
+double Spritverbrauch(double, double)
+{
+
+}
+
+double Tankinhalt(double, double, double)
+{
+
+}
